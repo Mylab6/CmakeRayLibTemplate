fe1812e3fdb00968663d668f506d08f4fd32f7b3
Keith <dreanihad@gmail.com>
2024-09-04 18:52:10 -0400

    a

--- main.c
+++ main.c
@@ -1,31 +1,38 @@
+// main.c
 #include "raylib.h"
-
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
+#include "gui.h"
 
 int main()
 {
-    InitWindow(400, 200, "raygui - controls test suite");
+    InitWindow(400, 200, "raygui - 45678 test suite");
     SetTargetFPS(60);
+ // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
     bool showMessageBox = false;
 
     while (!WindowShouldClose())
     {
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+        // Call the GUI rendering function
+        RenderGUI(&showMessageBox);
+        
+            BeginMode3D(camera);
 
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
 
-                if (result >= 0) showMessageBox = false;
-            }
+                DrawGrid(10, 1.0f);
+
+            EndMode3D();
 
         EndDrawing();
     }
