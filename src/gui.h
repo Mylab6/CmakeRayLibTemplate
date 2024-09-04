d89938250db9a7931acf44ae36ef03dbaae3d23a
Keith <dreanihad@gmail.com>
2024-09-04 18:32:32 -0400

    a

---
+++ src/gui.h
@@ -0,0 +1,10 @@
+// gui.h
+#ifndef GUI_H
+#define GUI_H
+
+#include "raylib.h"
+#include "raygui.h"
+
+void RenderGUI(bool *showMessageBox);
+
+#endif // GUI_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
+// main.c
 #include "raylib.h"
-
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
+#include "gui.h"
 
 int main()
 {
@@ -12,21 +11,12 @@ int main()
 
     while (!WindowShouldClose())
     {
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+        // Call the GUI rendering function
+        RenderGUI(&showMessageBox);
 
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
-
-                if (result >= 0) showMessageBox = false;
-            }
-    
         EndDrawing();
     }
 
