11876bfe7c5497ad380ee5308eee66c9c68c2c95
Keith <keith@localhost.localdomain>
2024-09-05 20:46:20 -0400

    Thu Sep  5 08:46:20 PM EDT 2024

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "raylib-cpp.hpp"
 #include "tank.h"
+raylib::Window  window; 
+Camera3D camera; 
+Tank* tank = nullptr;
+void UpdateDrawFrame(void);
 
 int main() {
     int screenWidth = 800;
@@ -11,17 +15,31 @@ int main() {
 
     // Create a tank in the center of the screen
     Vector3 tankPosition = {0.0f, 0.0f, 0.0f};
-    Tank tank(tankPosition, 0.0f, 1.0f, 1.0f, RED);
+    tank =  new Tank(tankPosition, 0.0f, 1.0f, 1.0f, RED);
 
     // Camera setup
-    Camera3D camera = { 0 };
+    camera = { 0 };
     camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    while (!window.ShouldClose()) {
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+    
+
+    return 0;
+}
+void UpdateDrawFrame(void){
+
         // Update camera
         UpdateCamera(&camera, CAMERA_ORBITAL);
 
@@ -33,7 +51,7 @@ int main() {
         BeginMode3D(camera);
 
         // Draw the tank
-        tank.Draw();
+        tank->Draw();
 
         // Draw a grid
         DrawGrid(20, 1.0f);
@@ -44,6 +62,3 @@ int main() {
 
         EndDrawing();
     }
-
-    return 0;
-}
